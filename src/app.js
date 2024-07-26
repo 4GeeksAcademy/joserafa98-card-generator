b80e79697dd2ec5219c22e73d9833c74e9164124
Joserafa98 <JOSERAFAHERNANDEZ.98@GMAIL.COM>
2024-07-26 18:29:25 +0000

    fixing bugs

--- src/app.js
+++ src/app.js
@@ -6,50 +6,43 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  function pokerCardRandom() {
-    let pokerCardOptions = [
-      "2",
-      "3",
-      "4",
-      "5",
-      "6",
-      "7",
-      "8",
-      "9",
-      "10",
-      "J",
-      "Q",
-      "K",
-      "A"
-    ];
-    let randomResult = Math.floor(Math.random() * pokerCardOptions.length);
-    let pokerCardNumber = pokerCardOptions[randomResult];
-    document.querySelector("#pokerCardNumber").innerHTML = pokerCardNumber;
-  }
-  function pokerCardIcon() {
-    let pokerCardIcons = [
-      { icon: "♦", color: "red" },
-      { icon: "♥", color: "red" },
-      { icon: "♠", color: "black" },
-      { icon: "♣", color: "black" }
-    ];
-    let pokerCardIconsRandom = Math.floor(
-      Math.random() * pokerCardIcons.length
-    );
-    let selectedIcon = pokerCardIcons[pokerCardIconsRandom];
-    document.querySelector("#pokerCardIcon").innerHTML = selectedIcon.icon;
-    document.querySelector("#pokerCardIcon").style.color = selectedIcon.color;
-    document.querySelector("#samePokerIcon").innerHTML = selectedIcon.icon;
-    document.querySelector("#samePokerIcon").style.color = selectedIcon.color;
-  }
-  function generateNewCard() {
-    pokerCardRandom();
-    pokerCardIcon();
-  }
-  generateNewCard();
-  setInterval(generateNewCard(), 3000);
+  pokerCardRandom();
+  setInterval(pokerCardRandom, 3000);
 };
+function pokerCardRandom() {
+  let pokerCardOptions = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+  let randomResult = Math.floor(Math.random() * pokerCardOptions.length);
+  let pokerCardNumber = pokerCardOptions[randomResult];
+  document.querySelector("#pokerCardNumber").innerHTML = pokerCardNumber;
 
+  let pokerCardIcons = [
+    { icon: "♦", color: "red" },
+    { icon: "♥", color: "red" },
+    { icon: "♠", color: "black" },
+    { icon: "♣", color: "black" }
+  ];
+  let pokerCardIconsRandom = Math.floor(Math.random() * pokerCardIcons.length);
+  let selectedIcon = pokerCardIcons[pokerCardIconsRandom];
+  document.querySelector("#pokerCardIcon").innerHTML = selectedIcon.icon;
+  document.querySelector("#pokerCardIcon").style.color = selectedIcon.color;
+  document.querySelector("#samePokerIcon").innerHTML = selectedIcon.icon;
+  document.querySelector("#samePokerIcon").style.color = selectedIcon.color;
+}
+document.querySelector("#newCard").addEventListener("click", pokerCardRandom);
 //pseudocodigo:
 //1.generar numeros al azar
 //2. imprimirlo en el html
